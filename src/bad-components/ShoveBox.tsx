8c62cf8fabd019de56f934a72d2fd1ee8f760131
Derek Johnson <howpopsir@gmail.com>
2024-10-20 15:22:52 -0400

    Finished ColoredBox and ShoveBox.

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -11,7 +11,7 @@ function ShoveBoxButton({
     return (
         <Button
             onClick={() => {
-                setPosition(4 + position);
+                setPosition(position);
             }}
         >
             Shove the Box
@@ -19,8 +19,7 @@ function ShoveBoxButton({
     );
 }
 
-function MoveableBox(): React.JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position }: { position: number }): React.JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -38,19 +37,22 @@ function MoveableBox(): React.JSX.Element {
 }
 
 export function ShoveBox(): React.JSX.Element {
-    const box = MoveableBox();
+    const [position, setPosition] = useState(10);
+    const box = MoveableBox({ position });
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={() => {
+                        setPosition(4 + position);
+                    }}
                 ></ShoveBoxButton>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
